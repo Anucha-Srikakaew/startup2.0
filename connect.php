2e33eaf97f095d986bc82fa39f81ae8d3c7437d5
Anucha Srilakaew <anucha.srilakaew@sony.com>
2023-01-30 11:01:25 +0700

    debug connect odbc.

--- connect.php
+++ connect.php
@@ -17,20 +17,20 @@ if (mysqli_connect_errno()) {
 }
 
 // MSSQL MEMBER STTC
-$serverName = "43.72.52.158";
-$database = "STTC_HUMAN_RESOURCE";
-$uid = 'mfe';
-$pwd = 'mfeP@ssw0rd';
-try {
-	$conn = new PDO(
-		"sqlsrv:server=$serverName;Database=$database",
-		$uid,
-		$pwd,
-		array(
-			//PDO::ATTR_PERSISTENT => true,
-			PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION
-		)
-	);
-} catch (PDOException $e) {
-	die("Error connecting to SQL Server: " . $e->getMessage());
-}
+// $serverName = "43.72.52.158";
+// $database = "STTC_HUMAN_RESOURCE";
+// $uid = 'mfe';
+// $pwd = 'mfeP@ssw0rd';
+// try {
+// 	$conn = new PDO(
+// 		"sqlsrv:server=$serverName;Database=$database",
+// 		$uid,
+// 		$pwd,
+// 		array(
+// 			//PDO::ATTR_PERSISTENT => true,
+// 			PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION
+// 		)
+// 	);
+// } catch (PDOException $e) {
+// 	die("Error connecting to SQL Server: " . $e->getMessage());
+// }
